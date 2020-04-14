a7cb0d8b11ae9347ea14429b3550df196a728cf4
Iñaki <correode.iz@gmail.com>
2020-04-14 14:17:40 +0200

    Añadido boton de pruebas y creado fragment visualizacion

---
+++ app/src/main/java/com/stirling/stfloor/ui/visualizacion/VisualizacionFragment.java
@@ -0,0 +1,35 @@
+package com.stirling.stfloor.ui.visualizacion;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.stirling.stfloor.R;
+
+public class VisualizacionFragment extends Fragment {
+
+    private VisualizacionViewModel mViewModel;
+
+    public static VisualizacionFragment newInstance() {
+        return new VisualizacionFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.visualizacion_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(VisualizacionViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/stirling/stfloor/ui/visualizacion/VisualizacionViewModel.java
@@ -0,0 +1,7 @@
+package com.stirling.stfloor.ui.visualizacion;
+
+import android.arch.lifecycle.ViewModel;
+
+public class VisualizacionViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
